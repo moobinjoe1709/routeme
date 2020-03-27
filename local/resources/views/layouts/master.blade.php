fa35ae3c6a91a82c9f9ee981391a706e7b80bac0
moobinjoe1709 <j.patipan.tongsang@gmail.com>
2020-03-27 17:30:33 +0700

    start project

---
+++ local/resources/views/layouts/master.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+         <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <title>@yield('title')</title>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+        <!-- manifest -->
+        <link rel="manifest" href="{{url('asset/manifest.json')}}">
+
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="application-name" content="RouteMe">
+        <meta name="apple-mobile-web-app-title" content="RouteMe">
+        <meta name="msapplication-starturl" content="/">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <link rel="icon" type="image/png" sizes="192x192" href="{{url('images/homescreen.png')}}">
+        <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="{{url('images/homescreen.png')}}">
+
+
+        @yield('css')
+    </head>
+    <body>
+      
+        <nav class="navbar navbar-expand-md navbar-light bg-master shadow-sm">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 col-md-12">
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <img src="{{asset('images/journeys_logoweb.png')}}" width="50%"/>
+                            {{-- {{ config('app.name', 'Laravel') }} --}}
+                        </a>
+                      
+                    </div>
+                    <div class="col-6 col-md-6 text-right">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                </div>
+    
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+    
+                    </ul>
+    
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('messages.login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('messages.register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+    
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+    
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    
+        
+        @yield('content')
+    </body>
+
+@yield('js')
+</html>
